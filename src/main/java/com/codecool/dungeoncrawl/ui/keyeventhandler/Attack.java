6fa6040a0cd0541c5b91c108bd9fd197864e6eda
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2024-09-10 21:51:25 +0200

    rewriting attack event handler

--- src/main/java/com/codecool/dungeoncrawl/ui/keyeventhandler/Attack.java
+++ src/main/java/com/codecool/dungeoncrawl/ui/keyeventhandler/Attack.java
@@ -2,6 +2,7 @@ package com.codecool.dungeoncrawl.ui.keyeventhandler;
 
 import com.codecool.dungeoncrawl.data.Cell;
 import com.codecool.dungeoncrawl.data.GameMap;
+import com.codecool.dungeoncrawl.data.actors.Actor;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
@@ -17,15 +18,31 @@ public class Attack implements KeyHandler{
     neighbouringCells[2] = playerCell.getNeighbor(0, 1);
     neighbouringCells[3] = playerCell.getNeighbor(0, -1);
 
-    for (Cell neighbouringCell : neighbouringCells) {
+    /*for (Cell neighbouringCell : neighbouringCells) {
       if (code.equals(event.getCode())) {
         if (neighbouringCell.getActor() != null) {
           map.getPlayer().attack(neighbouringCell.getActor());
+          break;
+        } else {
+          System.out.println("There's no one near to attack. Don't fos!");
         }
-
       }
+    }*/
 
-
+    Actor monster = null;
+    if (code.equals(event.getCode())) {
+      for (Cell neighbouringCell : neighbouringCells) {
+        if (neighbouringCell.getActor() != null) {
+          monster = neighbouringCell.getActor();
+        }
+      }
+      if (monster != null) {
+        map.getPlayer().attack(monster);
+        System.out.println("Monster HP: " + monster.getHealth());
+        System.out.println("Player HP: " + map.getPlayer().getHealth());
+      } else {
+        System.out.println("There's no one near to attack. Don't fos!");
+      }
     }
 
   }
