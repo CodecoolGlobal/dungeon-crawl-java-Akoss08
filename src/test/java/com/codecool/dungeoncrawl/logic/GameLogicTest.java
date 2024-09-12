1cfb5b47d5c1c0fa6879e82550d6ce6683f2060f
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2024-09-12 14:32:25 +0200

    adding testcase for returning null when no neighbouring monster found

--- src/test/java/com/codecool/dungeoncrawl/logic/GameLogicTest.java
+++ src/test/java/com/codecool/dungeoncrawl/logic/GameLogicTest.java
@@ -10,6 +10,7 @@ import com.codecool.dungeoncrawl.data.mapElements.actors.monsters.Spider;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class GameLogicTest {
   GameMap map = new GameMap(3, 3, CellType.FLOOR);
@@ -18,11 +19,20 @@ public class GameLogicTest {
   void getNeighbouringMonster() {
     Player player = new Player(map.getCell(1, 1));
     map.setPlayer(player);
-    //Cell playerCell = map.getPlayer().getCell();
     Monster monster = new Spider(map.getCell(1, 0));
 
     Actor result = map.getMonster();
     assertEquals(1, result.getX());
     assertEquals(0, result.getY());
+  }
+
+  @Test
+  void getMonsterReturnsNullWhenNoNeighbouringMonster() {
+    Player player = new Player(map.getCell(1, 1));
+    map.setPlayer(player);
+    //Monster monster = new Spider(map.getCell(2, 2));
+
+    Actor result = map.getMonster();
+    assertNull(result);
   }
 }
