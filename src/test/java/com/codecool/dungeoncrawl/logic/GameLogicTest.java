dd23a7e1316965ae8d0966d7dc8623f668126021
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2024-09-12 14:21:36 +0200

    adding testcase for getting neighbouring monster

--- src/test/java/com/codecool/dungeoncrawl/logic/GameLogicTest.java
+++ src/test/java/com/codecool/dungeoncrawl/logic/GameLogicTest.java
@@ -1,2 +1,28 @@
-package com.codecool.dungeoncrawl.logic;public class GameLogicTest {
+package com.codecool.dungeoncrawl.logic;
+
+import com.codecool.dungeoncrawl.data.Cell;
+import com.codecool.dungeoncrawl.data.CellType;
+import com.codecool.dungeoncrawl.data.GameMap;
+import com.codecool.dungeoncrawl.data.mapElements.actors.Actor;
+import com.codecool.dungeoncrawl.data.mapElements.actors.Player;
+import com.codecool.dungeoncrawl.data.mapElements.actors.monsters.Monster;
+import com.codecool.dungeoncrawl.data.mapElements.actors.monsters.Spider;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GameLogicTest {
+  GameMap map = new GameMap(3, 3, CellType.FLOOR);
+
+  @Test
+  void getNeighbouringMonster() {
+    Player player = new Player(map.getCell(1, 1));
+    map.setPlayer(player);
+    //Cell playerCell = map.getPlayer().getCell();
+    Monster monster = new Spider(map.getCell(1, 0));
+
+    Actor result = map.getMonster();
+    assertEquals(1, result.getX());
+    assertEquals(0, result.getY());
+  }
 }
