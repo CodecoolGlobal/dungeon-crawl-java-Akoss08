d1c21c93ef1a0064d94134ac61eed11e9b27b1f4
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2024-09-12 19:54:48 +0200

    implementing unit test for getPlayerStrength

--- src/test/java/com/codecool/dungeoncrawl/logic/GameLogicTest.java
+++ src/test/java/com/codecool/dungeoncrawl/logic/GameLogicTest.java
@@ -2,15 +2,24 @@ package com.codecool.dungeoncrawl.logic;
 
 import com.codecool.dungeoncrawl.data.CellType;
 import com.codecool.dungeoncrawl.data.GameMap;
+import com.codecool.dungeoncrawl.data.mapElements.actors.Player;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameLogicTest {
   GameMap map = new GameMap(3, 3, CellType.FLOOR);
+  GameLogicFake logic = new GameLogicFake(map);
 
   @Test
   void getPlayerStrength() {
+    Player player = new Player(map.getCell(1,1));
+    map.setPlayer(player);
+
+    String expected = "5";
+    String playerStrength = logic.getPlayerStrength();
+
+    assertEquals(expected, playerStrength);
   }
 
   @Test
@@ -31,5 +40,6 @@ class GameLogicTest {
 
   @Test
   void moveMonsters() {
+
   }
 }
