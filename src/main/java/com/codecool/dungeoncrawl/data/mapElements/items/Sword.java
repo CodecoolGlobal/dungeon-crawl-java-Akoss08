57847fa30bfd8bf41ddf9609998d5819b5b81518
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2024-09-14 13:45:09 +0200

    implement sword usage

--- src/main/java/com/codecool/dungeoncrawl/data/mapElements/items/Sword.java
+++ src/main/java/com/codecool/dungeoncrawl/data/mapElements/items/Sword.java
@@ -4,19 +4,18 @@ import com.codecool.dungeoncrawl.data.Cell;
 import com.codecool.dungeoncrawl.data.mapElements.actors.Player;
 
 public class Sword extends Item {
+    private static final String TILE_NAME = "sword";
+    private static final int PLUS_ATTACK = 2;
+    private static final String TILE_NAME_FOR_PLAYER = "playerWithSword";
 
     public Sword(Cell cell) {
-        super(cell);
+        super(cell, TILE_NAME);
     }
 
     @Override
-    public String getTileName() {
-        return "sword";
-    }
-
-    @Override
-    public void setAbility(Player player) {
-        int plusAttack = 2;
-        player.setAttackStrength(player.getAttackStrength() + plusAttack);
+    public void addToPlayer(Player player) {
+        player.getInventory().addItem(this);
+        player.setAttackStrength(player.getAttackStrength() + PLUS_ATTACK);
+        player.setTileName(TILE_NAME_FOR_PLAYER);
     }
 }
