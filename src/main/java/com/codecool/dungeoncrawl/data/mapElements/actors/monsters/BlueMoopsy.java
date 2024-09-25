99d5b482125a1d02c0eacb71daa6bfabc2cc1bea
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2024-09-25 21:28:40 +0200

    creating BlueMoopsy class - in progress

---
+++ src/main/java/com/codecool/dungeoncrawl/data/mapElements/actors/monsters/BlueMoopsy.java
@@ -0,0 +1,26 @@
+package com.codecool.dungeoncrawl.data.mapElements.actors.monsters;
+
+import com.codecool.dungeoncrawl.data.Cell;
+
+public class BlueMoopsy extends Monster {
+  private static int baseHealth;
+  private static final int BASE_POWER = 4;
+  private static final int XP_VALUE = 5;
+  private static final String ABILITY =
+          "When Moopsy's HP is half of the starting rate it divides into two Moopsies... " +
+                  "\nuntil it dies. \nOh, and they can still teleport.";
+  private static final String[] TILE_NAMES = {
+          "blueMoopsy1",
+          "blueMoopsy2",
+          "blueMoopsy3",
+          "blueMoopsy4",
+          "blueMoopsy5",
+          "blueMoopsy6"
+  };
+
+  public BlueMoopsy(Cell cell, int moopsyHPBeforeSplit) {
+    baseHealth = moopsyHPBeforeSplit;
+    super(cell, baseHealth, BASE_POWER, ABILITY, TILE_NAMES[0], XP_VALUE);
+
+  }
+}
--- src/main/java/com/codecool/dungeoncrawl/data/mapElements/actors/monsters/Moopsy.java
+++ src/main/java/com/codecool/dungeoncrawl/data/mapElements/actors/monsters/Moopsy.java
@@ -8,9 +8,22 @@ public class Moopsy extends Monster {
   private static final int XP_VALUE = 10;
   private static final String ABILITY =
           "When Moopsy's HP is half of the starting rate it divides into two Moopsies. Oh, and they can also teleport.";
-  private static final String TILE_NAME = "moopsy";
+  private static final String TILE_NAME = "yellowMoopsy1";
+  private static final String[] TILE_NAMES = {
+          "yellowMoopsy1",
+          "yellowMoopsy2",
+          "yellowMoopsy3",
+          "yellowMoopsy4",
+          "yellowMoopsy5",
+          "yellowMoopsy6"
+  };
 
   public Moopsy(Cell cell) {
-    super(cell, BASE_HEALTH, BASE_POWER, ABILITY, TILE_NAME, XP_VALUE);
+    super(cell, BASE_HEALTH, BASE_POWER, ABILITY, TILE_NAMES[0], XP_VALUE);
   }
+
+  /*@Override
+  public String getTileName() {
+
+  }*/
 }
