b924b1b65f755d169af827a78b81c3a8aeea4238
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2024-09-25 17:31:50 +0200

    implementing effect handling logic

--- src/main/java/com/codecool/dungeoncrawl/data/mapElements/actors/monsters/RatBoss.java
+++ src/main/java/com/codecool/dungeoncrawl/data/mapElements/actors/monsters/RatBoss.java
@@ -1,6 +1,9 @@
 package com.codecool.dungeoncrawl.data.mapElements.actors.monsters;
 
 import com.codecool.dungeoncrawl.data.Cell;
+import com.codecool.dungeoncrawl.data.mapElements.actors.Effect;
+import com.codecool.dungeoncrawl.data.mapElements.actors.Player;
+import com.codecool.dungeoncrawl.data.mapElements.actors.WeakenEffect;
 
 import java.util.Random;
 
@@ -11,6 +14,7 @@ public class RatBoss extends Monster {
   private static final String ABILITY =
           "Rats are sneaky and can disappear from sight, also spreading disease weakening the player by 2";
   private static final String TILE_NAME = "ratBoss";
+  private static final Effect WEAKEN = new WeakenEffect(1,2);
 
   public RatBoss(Cell cell) {
     super(cell, BASE_HEALTH, BASE_POWER, ABILITY, TILE_NAME, XP_VALUE);
@@ -24,6 +28,26 @@ public class RatBoss extends Monster {
     } else {
       return TILE_NAME;
     }
+  }
+
+  @Override
+  public void attack(Player player) {
+    if (health <= 0) {
+      die();
+      player.collectXp(XP_VALUE);
+    } else {
+      weaken(player);
+
+      int playerHealth = player.getHealth();
+      int monsterStrength = Math.max(attackStrength - player.getDefense(), 0);
+      int playerNewHealth = playerHealth - monsterStrength;
+
+      player.setHealth(playerNewHealth);
+    }
+  }
+
+  private void weaken(Player player) {
+    player.applyEffect(WEAKEN);
   }
 
 }
