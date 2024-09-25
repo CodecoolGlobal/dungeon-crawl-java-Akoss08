da0ddd4cb5a802f06e390dfe7e6dcc6421577ed1
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2024-09-25 11:12:16 +0200

    adding snakes and a guard

--- src/main/java/com/codecool/dungeoncrawl/logic/MapLoader.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/MapLoader.java
@@ -14,7 +14,7 @@ import java.io.InputStream;
 import java.util.Scanner;
 
 public class MapLoader {
-    private static String fileName = "/map2.txt";
+    private static String fileName = "/map1.txt";
     private static Player existingPlayer;
 
     public static void setFileName(String fileName) {
@@ -154,7 +154,7 @@ public class MapLoader {
                             cell.setType(CellType.FLOOR);
                             map.addNpc(new BridgeGuard(cell));
                             break;
-                        case 'P':
+                        case 'a':
                             cell.setType(CellType.FLOOR);
                             Snake snake = new Snake(cell, new SnakeTooth());
                             map.addMonster(snake);
