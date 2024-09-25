8e931bbb85631f54f30b9a0952f21929a77af892
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2024-09-25 23:05:12 +0200

    implementing moopsy animation

--- src/main/java/com/codecool/dungeoncrawl/data/mapElements/actors/monsters/YellowMoopsy.java
+++ src/main/java/com/codecool/dungeoncrawl/data/mapElements/actors/monsters/YellowMoopsy.java
@@ -8,24 +8,32 @@ public class YellowMoopsy extends Moopsy {
   private static final int XP_VALUE = 10;
   private static final String ABILITY =
           "When Moopsy's HP is half of the starting rate it divides into two Moopsies. Oh, and they can also teleport.";
-  private static final String TILE_NAME = "yellowMoopsy1";
   private static final String[] TILE_NAMES = {
           "yellowMoopsy1",
           "yellowMoopsy2",
           "yellowMoopsy3",
           "yellowMoopsy4",
           "yellowMoopsy5",
-          "yellowMoopsy6"
+          //"yellowMoopsy6"
   };
+
+  private int tileIndex = 0;
 
   public YellowMoopsy(Cell cell) {
     super(cell, BASE_HEALTH, BASE_POWER, ABILITY, TILE_NAMES[0], XP_VALUE);
   }
 
-  /*@Override
+  @Override
   public String getTileName() {
+    String currentTileName = TILE_NAMES[tileIndex];
 
-  }*/
+    tileIndex = (tileIndex + 1) % TILE_NAMES.length;
+    return currentTileName;
+  }
+
+  public void split() {
+
+  }
 
 
 }
